b177bc4b95af8bd305c884af45f3c570f1e9b045
AbhinavJawle <jawleabhinav7@gmail.com>
2025-05-16 23:01:46 +0530

    fix: update dodopayments env

--- lib/dodopayments.ts
+++ lib/dodopayments.ts
@@ -1,5 +1,9 @@
 import DodoPayments from "dodopayments";
 export const dodopayments = new DodoPayments({
-  bearerToken: process.env.DODO_API_KEY_TEST, // This is the default and can be omitted if env is named as DODO_PAYMENTS_API_KEY
-  environment: "test_mode", // defaults to 'live_mode'
+  bearerToken:
+    process.env.NODE_ENV === "development"
+      ? process.env.DODO_PAYMENTS_API_TEST_KEY
+      : process.env.DODO_PAYMENTS_API_LIVE_KEY,
+  environment:
+    process.env.NODE_ENV === "development" ? "test_mode" : "live_mode",
 });

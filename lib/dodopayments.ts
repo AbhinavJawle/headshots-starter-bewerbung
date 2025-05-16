f3e18d1441deaeeeda6367d0e56aaec10ab667df
AbhinavJawle <jawleabhinav7@gmail.com>
2025-05-16 23:20:52 +0530

    fix: env variables

--- lib/dodopayments.ts
+++ lib/dodopayments.ts
@@ -2,8 +2,8 @@ import DodoPayments from "dodopayments";
 export const dodopayments = new DodoPayments({
   bearerToken:
     process.env.NODE_ENV === "development"
-      ? process.env.DODO_PAYMENTS_API_TEST_KEY
-      : process.env.DODO_PAYMENTS_API_LIVE_KEY,
+      ? process.env.DODO_API_KEY_TEST
+      : process.env.DODO_API_KEY_LIVE,
   environment:
     process.env.NODE_ENV === "development" ? "test_mode" : "live_mode",
 });
